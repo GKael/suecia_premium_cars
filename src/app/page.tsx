85af94dcd874eb720be6da8ae2452e03c575c49a
Suécia Deploy <deploy@sueciapremium.com>
2026-09-15 10:57:12 -0300

    Fix iframe props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -381,7 +381,7 @@ export default function Home() {
                     width="100%" 
                     height="100%" 
                     style={{ border: 0 }} 
-                    allowfullscreen="" 
+                    allowFullScreen 
                     loading="lazy">
                 </iframe>
                 
@@ -474,5 +474,6 @@ export default function Home() {
     </>
   );
 }
+
 
 
